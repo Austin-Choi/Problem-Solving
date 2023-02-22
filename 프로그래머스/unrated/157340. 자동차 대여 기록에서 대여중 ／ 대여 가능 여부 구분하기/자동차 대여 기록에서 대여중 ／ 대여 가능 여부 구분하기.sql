e17f636d5901173f51d70fa22108d634d73fe02e
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-02-22 09:22:38 +0900

    [unrated] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,6 +1,9 @@
 -- 코드를 입력하세요
 -- case문 사용법 
 -- case로 시작, when으로 조건절, then 조건절이 참일 때 실행할 것, else 조건절이 거짓일때 실행, end로 종료
+-- max를 쓰는 이유는
+-- 같은 id를 가지는 차가 여러 대여 이력이 있을 때, 최근 이력으로 가져와야 하므로
+-- max(날짜) -> 최근, min(날짜) -> 가장 오래된
 SELECT car_id, max(
     case
     when '2022-10-16' between date_format(start_date, '%Y-%m-%d') and date_format(end_date, '%Y-%m-%d')
