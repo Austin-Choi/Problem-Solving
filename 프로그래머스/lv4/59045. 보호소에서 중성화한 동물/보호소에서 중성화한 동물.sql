4a0a658840e11ebb9ed8e367a30b3666999c06d2
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-02-23 18:36:06 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+select a.animal_id, a.animal_type, a.name
+from animal_ins a
+    join animal_outs b
+    on a.animal_id = b.animal_id
+where a.sex_upon_intake like 'intact%' 
+    and b.sex_upon_outcome not like 'intact%'
