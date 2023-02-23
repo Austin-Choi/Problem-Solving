73a2f9505439009643dfd00607b1106c42db6cca
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-02-24 08:50:59 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+long long dp[45];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	dp[0] = 0;
+	dp[1] = 1;
+	short n;
+	cin >> n;
+	for (short i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	cout << dp[n];
+	return 0;
+}
