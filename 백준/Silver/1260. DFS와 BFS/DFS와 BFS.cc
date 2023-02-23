d0670125b0891ce4aaf141c5c28e9df2d268bb30
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-02-23 11:14:34 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -6,59 +6,96 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> vec[10002];
-vector<int> v_bfs;
-vector<int> v_dfs;
-bool visit[1002];
+vector<int> road[1001];
+bool visit[1001];
+//n 정점의 개수
+//m 간선의 개수
+//v 시작점
+//a, b 간선정보 (시작a, 끝b)
+//양방향이면 간선들[a][b]=1 간선들[b][a] =1로 처리
+int n, m, v, a, b;
 
-void bfs(int tmp) {
+//int road[시작점][끝점]
+//인접행렬을 이용, 재귀로 구현한 dfs
+void dfs_old(int v) {
+	visit[v] = true;
+	cout << v << " ";
+
+	for (int i = 1; i <= n; i++) {
+		if (road[v][i] == 1 && !visit[i]) {
+			visit[i] = true;
+			dfs_old(i);
+		}
+	}
+}
+
+//int road[시작점][끝점]
+//인접행렬을 이용, queue로 구현한 dfs
+void bfs_old(int v) {
 	queue<int> q;
-	q.push(tmp);
-	visit[tmp] = true;
+	q.push(v);
+	visit[v] = true;
+	cout << v << " ";
 
 	while (!q.empty()) {
-		int x = q.front();
+		v = q.front();
 		q.pop();
-		v_bfs.push_back(x);
 
-		for (size_t i = 0; i < vec[x].size(); i++) {
-			if (!visit[vec[x][i]]){
-				q.push(vec[x][i]);
-				//cout << vec[x][i] << " ";
-				visit[vec[x][i]] = true;
+
+		for (int i = 1; i <= n; i++) {
+			if (road[v][i] == 1 && !visit[i]) {
+				q.push(i);
+				visit[i] = true;
+				cout << i << " ";
 			}
-				
 		}
 	}
 }
 
-void dfs(int x) {
-	visit[x] = true;
-	v_dfs.push_back(x);
+//vector<int> road[시작점]={끝점1, 끝점2, 끝점3 ... } ;
+//인접 리스트를 이용, stack으로 구현한 dfs
+void dfs(int v) {
+	stack<int> s;
+	s.push(v);
 
-	for (size_t i = 0; i < vec[x].size(); i++) {
-		if (!visit[vec[x][i]]) {
-			//cout << vec[x][i] << " ";
-			dfs(vec[x][i]);
+	while (!s.empty()) {
+		int idx = s.top();
+		s.pop();
+
+		if (!visit[idx]) {
+			visit[idx] = true;
+			cout << idx << " ";
+
+			//dfs는 뒤에서부터 스택에 삽입
+			//stack은 후입선출의 구조라
+			for (int i = road[idx].size()-1; i >= 0; i--) {
+				if (!visit[road[idx][i]]) {
+					s.push(road[idx][i]);
+				}
+			}
 		}
 	}
 }
 
-void dfs2(int tmp) {
-	stack<int> stk;
-	stk.push(tmp);
-	visit[tmp] = true;
-	v_dfs.push_back(tmp);
+//vector<int> road[시작점]={끝점1, 끝점2, 끝점3 ... } ;
+//인접 리스트를 이용, queue으로 구현한 bfs
+void bfs(int v) {
+	queue<int> q;
+	q.push(v);
 
-	while (!stk.empty()) {
-		int x = stk.top();
-		stk.pop();
-		
-		
-		for (size_t i = 0; i < vec[x].size(); i++) {
-			if (!visit[vec[x][i]]) {
-				stk.push(x);
-				visit[vec[x][i]] = true;
+	while (!q.empty()) {
+		int idx = q.front();
+		q.pop();
+
+		if (!visit[idx]) {
+			visit[idx] = true;
+			cout << idx << " ";
+
+			//queue는 선입선출의 구조
+			for (int i = 0; i < road[idx].size(); i++) {
+				if (!visit[road[idx][i]]) {
+					q.push(road[idx][i]);
+				}
 			}
 		}
 	}
@@ -66,25 +103,28 @@ void dfs2(int tmp) {
 
 
 int main() {
-	int n, m, v, a, b;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
 	cin >> n >> m >> v;
-	for (int i = 1; i < m + 1; i++) {
+	//n = 4;
+	//m = 5;
+	//v = 1;
+	for (int i = 0; i < m; i++) {
 		cin >> a >> b;
-		vec[a].push_back(b);
-		vec[b].push_back(a); //양방향 간선처리
+		road[a].push_back(b);
+		road[b].push_back(a); //양방향 간선처리
 	}
-	for (int i = 1; i < n + 1; i++) {
-		sort(vec[i].begin(), vec[i].end());
+	//sorting 
+	for (int i = 1; i <= n; i++) {
+		sort(road[i].begin(), road[i].end());
 	}
+	memset(visit, false, sizeof(visit));
 	dfs(v);
+	cout << "\n";
 	memset(visit, false, sizeof(visit));
 	bfs(v);
-	for (auto x : v_dfs) {
-		cout << x << " ";
-	}
-	cout << endl;
-	for (auto x : v_bfs) {
-		cout << x << " ";
-	}
+	
 	return 0;
 }
