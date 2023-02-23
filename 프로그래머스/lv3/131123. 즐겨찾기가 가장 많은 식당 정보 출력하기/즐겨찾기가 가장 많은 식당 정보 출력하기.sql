28eb5186fea26e81aeac2ed340fafb902f3c5462
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-02-23 18:32:44 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,6 +1,8 @@
 -- 코드를 입력하세요
 select food_type, rest_id, rest_name, favorites
 from rest_info
+-- 그냥 서브쿼리 없이 처리하면 max(fav)에 동일한 값이 여러개 나올수 있어서 
+-- where 절에서 처리
 where (food_type, favorites) in
     (select food_type, max(favorites)
     from rest_info
