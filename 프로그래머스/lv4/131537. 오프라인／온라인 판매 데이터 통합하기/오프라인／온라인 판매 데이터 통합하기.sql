256d7e767bdf198e16bf65ca4c0d86c93c48a6ea
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-02-25 08:05:29 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,6 +1,10 @@
 -- 코드를 입력하세요
 -- join은 수평결합
 -- union은 수직결합
+-- UNION (DISTINCT)
+-- 쿼리의 결과를 합친다. 중복된 ROW는 제거 
+-- UNION ALL   
+-- 모든 컬럼값이 같은 ROW도 결과로 보여준다. 중복제거 하지 않는다.
 select date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, user_id, sales_amount
 from online_sale
 where date_format(sales_date,'%Y-%m')='2022-03'
