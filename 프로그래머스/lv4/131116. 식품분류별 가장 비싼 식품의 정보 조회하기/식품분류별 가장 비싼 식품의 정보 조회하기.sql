e0e5dc324ef49e310ba3ab06554b42538250b3e9
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-02-25 08:09:06 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/lv4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,6 +1,12 @@
 -- 코드를 입력하세요
 -- https://jhnyang.tistory.com/328 where절 문자열조건
 -- where절에서 여러 조건을 만족할 때는 in 쓰기
+-- like는 한개랑 비교할 때
+-- "문자%" -> "문자열입니다." "문자열이에요."
+-- like문 와일드 카드
+-- % 0개 이상
+-- "문자_" -> "문자열", "문자수"
+-- _ 1개
 select category, price as max_price, product_name
 from food_product
 where price in 
