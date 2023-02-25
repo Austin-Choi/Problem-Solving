6a0724aaa6f27d0a52ce71083c3febd486f65495
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-02-25 09:18:18 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,6 +1,10 @@
 -- 코드를 입력하세요
 -- IFNULL(컬럼명, 대체할 값)함수
 -- IFNULL을 쓰면 WHERE절에 NULL인지 안 찾고도 변경 가능
-SELECT ANIMAL_TYPE, IFNULL(NAME, 'No name') AS NAME, SEX_UPON_INTAKE
+SELECT ANIMAL_TYPE, 
+-- IFNULL(NAME, 'No name') 
+    coalesce(name, 'No name')
+    AS NAME,
+    SEX_UPON_INTAKE
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID;
