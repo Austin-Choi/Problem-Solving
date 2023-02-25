5a6b5bc8d00b4ab2ff402985b15cd99c647995d8
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-02-25 09:15:49 +0900

    [level 1] Title: 경기도에 위치한 식품창고 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
+++ 프로그래머스/lv1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
@@ -5,12 +5,14 @@
 -- CASE 대신 coalesce()
 -- 
 SELECT WAREHOUSE_ID, WAREHOUSE_NAME, ADDRESS, 
-    (CASE
-        WHEN FREEZER_YN IS NULL THEN 'N'
-        ELSE FREEZER_YN
-    END)
+--    (CASE
+--    WHEN FREEZER_YN IS NULL THEN 'N'
+--    ELSE FREEZER_YN
+--    END)
+    coalesce(freezer_yn, 'N')
     AS FREEZER_YN
 FROM FOOD_WAREHOUSE
-WHERE SUBSTR(ADDRESS FROM 1 FOR 3) = '경기도'
+-- WHERE SUBSTR(ADDRESS FROM 1 FOR 3) = '경기도'
+where address like '경기도%'
 ORDER BY WAREHOUSE_ID ASC
 ;
