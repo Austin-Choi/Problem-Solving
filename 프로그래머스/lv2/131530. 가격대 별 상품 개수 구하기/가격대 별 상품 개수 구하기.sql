86bd880487f08690ee8e237e75558559d58948e4
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-02-25 09:10:02 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,6 +1,12 @@
 -- 코드를 입력하세요
-SELECT (PRICE DIV 10000) * 10000 AS PRICE_GROUP, COUNT(PRODUCT_ID) AS PRODUCTS
+-- mysql에서는 5/3을 하면 소숫점 자리까지 나온다 
+-- select truncate
+-- SELECT (PRICE DIV 10000) * 10000 
+-- select truncate(price/10000,0) * 10000 
+select floor(price/10000) * 10000
+    AS PRICE_GROUP, 
+    COUNT(PRODUCT_ID) AS PRODUCTS
 FROM PRODUCT
 GROUP BY PRICE_GROUP
-ORDER BY PRICE_GROUP ASC
+ORDER BY PRICE_GROUP
 ;
