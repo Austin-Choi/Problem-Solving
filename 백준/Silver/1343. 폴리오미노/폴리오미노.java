e85144cf298e74cf1e1d9bfe1007d6835ebe27ed
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-07-15 08:57:54 +0900

    [Silver V] Title: 폴리오미노, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		String str = st.nextToken();
+		//문자열 "XXXX"를 우선으로 찾아서 "AAAA"로 치환
+		str = str.replace("XXXX", "AAAA");
+		str = str.replace("XX", "BB");
+		
+		
+		//아직 남아있는 X는 치환되지 못한 문자열이 있는 것
+		if(str.contains("X"))
+			System.out.println(-1);
+		else
+			System.out.println(str);
+	}
+}
