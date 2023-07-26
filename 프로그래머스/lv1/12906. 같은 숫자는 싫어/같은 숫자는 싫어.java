51d60b2289f5061c4a68e676beaf7bb649ecf7cf
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-07-26 22:05:07 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 111.81 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Solution {
     public ArrayList<Integer> solution(int []arr) {
         ArrayList<Integer> answer = new ArrayList<>();
+        //순서대로 큐에 넣고 중복없이 answer에 저장
         Queue<Integer> q = new LinkedList<>();
         for(int i : arr){
             q.add(i);
