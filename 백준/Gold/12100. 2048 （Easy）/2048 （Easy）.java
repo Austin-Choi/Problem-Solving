6d1f606c0ec972e570900fbb19c92b0a214e553e
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-11-07 09:50:13 +0900

    [Gold II] Title: 2048 (Easy), Time: 184 ms, Memory: 20756 KB -BaekjoonHub

---
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -0,0 +1,140 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static int N, answer, map[][];
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        answer = 0;
+        map = new int[N][N];
+        StringTokenizer st;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++)
+                map[i][j] = Integer.parseInt(st.nextToken());
+        }
+        
+        dfs(0);
+        System.out.println(answer);
+    }
+    
+    public static void dfs(int depth) {
+        if(depth == 5) {
+            findMax();
+            return;
+        }
+        int copy[][] = new int[N][N];
+        for(int i = 0; i < N; i++)
+            copy[i] = map[i].clone();
+        
+        for(int i = 0; i < 4; i++) {
+            move(i);
+            dfs(depth+1);
+            for(int j = 0; j < N; j++)
+                map[j] = copy[j].clone();
+        }
+    }
+    
+    public static void move(int dir) {
+        switch(dir) {
+            //up
+            case 0:
+                for(int i = 0; i < N; i++) {
+                    int index = 0;
+                    int block = 0;
+                    for(int j = 0; j < N; j++) {
+                        if(map[j][i] != 0) {
+                            if(block == map[j][i]) {
+                                map[index - 1][i] = block * 2;
+                                block = 0;
+                                map[j][i] = 0;
+                            }
+                            else {
+                                block = map[j][i];
+                                map[j][i] = 0;
+                                map[index][i] = block;
+                                index++;
+                            }
+                        }
+                    }
+                }
+                break;
+            //down
+            case 1:
+                for(int i = 0; i < N; i++) {
+                    int index = N - 1;
+                    int block = 0;
+                    for(int j = N - 1; j >= 0; j--) {
+                        if(map[j][i] != 0) {
+                            if(block == map[j][i]) {
+                                map[index + 1][i] = block * 2;
+                                block = 0;
+                                map[j][i] = 0;
+                            }
+                            else {
+                                block = map[j][i];
+                                map[j][i] = 0;
+                                map[index][i] = block;
+                                index--;
+                            }
+                        }
+                    }
+                }
+                break;
+            //left
+            case 2:
+                for(int i = 0; i < N; i++) {
+                    int index = 0;
+                    int block = 0;
+                    for(int j = 0; j < N; j++) {
+                        if(map[i][j] != 0) {
+                            if(block == map[i][j]) {
+                                map[i][index - 1] = block * 2;
+                                block = 0;
+                                map[i][j] = 0;
+                            }
+                            else {
+                                block = map[i][j];
+                                map[i][j] = 0;
+                                map[i][index] = block;
+                                index++;
+                            }
+                        }
+                    }
+                }
+                break;
+            //right
+            case 3:
+                for(int i = 0; i < N; i++) {
+                    int index = N - 1;
+                    int block = 0;
+                    for(int j = N - 1; j >= 0; j--) {
+                        if(map[i][j] != 0) {
+                            if(block == map[i][j]) {
+                                map[i][index + 1] = block * 2;
+                                block = 0;
+                                map[i][j] = 0;
+                            }
+                            else {
+                                block = map[i][j];
+                                map[i][j] = 0;
+                                map[i][index] = block;
+                                index--;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
+    
+    public static void findMax() {
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+                answer = Math.max(answer, map[i][j]);
+    }
+}
