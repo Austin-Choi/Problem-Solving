7f0f002c8772fa30d094f084a0ff08a18dfb7d3f
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2024-06-08 17:46:59 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 6.97 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,28 +1,12 @@
 class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
+        myString = myString.toLowerCase();
+        pat = pat.toLowerCase();
         
-        String[] l1 = myString.split("");
-        String[] l2 = pat.split("");
-        
-        for(int i = 0; i<myString.length(); i++){
-            l1[i] = l1[i].toLowerCase();
-        }
-        for(int i = 0; i<pat.length(); i++){
-            l2[i] = l2[i].toLowerCase();
-        }
-        for(int i = 0; i<myString.length(); i++){
-            sb1.append(l1[i]);
-        }
-        for(int i = 0; i<pat.length(); i++){
-            sb2.append(l2[i]);
-        }
-        
-        if(sb1.toString().contains(sb2.toString()))
+        if(myString.indexOf(pat) != -1)
             return 1;
-        else 
+        else
             return 0;
     }
 }
