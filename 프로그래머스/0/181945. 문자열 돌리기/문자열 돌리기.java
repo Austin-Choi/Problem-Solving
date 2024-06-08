740a664635bcaaa23ba276339ec2607343074b66
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2024-06-08 20:53:08 +0900

    [level 0] Title: 문자열 돌리기, Time: 145.01 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] ls = s.split("");
+        StringBuilder sb = new StringBuilder();
+        
+        for(String ss : ls){
+            System.out.println(ss);
+        }
+        //System.out.print(sb.toString());
+    }
+}
