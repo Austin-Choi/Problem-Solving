3acbc87a91afe0cfa8f50e593c146b49af0befe7
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2024-06-09 18:33:27 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.12 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,22 +1,17 @@
 import java.util.*;
 
 class Solution {
-    public boolean check5N0(String s){
-        String[] ls = s.split("");
-        for(String ss : ls){
-            if(!(ss.equals("5") || ss.equals("0")))
-                return false;
-        }
-        return true;
-    }
     public List solution(int l, int r) {
         ArrayList<Integer> ans = new ArrayList<>();
-        for(;l<=r;l++){
-            if(l%5 == 0){
-                String s = String.valueOf(l);
-                if(check5N0(s))
-                    ans.add(l);
-            }
+        //결과는 1 10 11 100 101 110 ... 이진법으로 1 2 3 4 5 표현한것과 같음
+        // 1 000 000 은 이진법으로 2의 6승 64임
+        // Integer.toBinaryString 활용
+        for(int i = 1; i<=64; i++){
+            int n = Integer.parseInt(Integer.toBinaryString(i))*5;
+            if(l<=n && n<=r)
+                ans.add(n);
+            if(n>r)
+                break;
         }
         if(ans.isEmpty())
             ans.add(-1);
