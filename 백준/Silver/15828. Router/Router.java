999e6c4b05c506cbc592ce29cfaca241686147fe
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2024-08-26 16:54:01 +0900

    [Silver IV] Title: Router, Time: 896 ms, Memory: 50904 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15828. Router/Router.java
+++ 백준/Silver/15828. Router/Router.java
@@ -1,32 +1,33 @@
 import java.util.*;
 import java.io.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Queue<Integer> q = new LinkedList<>();
+        // BlockingQueue를 쓰면 사이즈 체크 안해도 됨
+        // 가득 차면 스레드를 Block 해서 offer하려 하면 false가 반환되기 때문에
+        Queue<Integer> q = new LinkedBlockingQueue<>(N);
 
         while(true){
             int temp = Integer.parseInt(br.readLine());
-            if(temp == -1){
+            if(temp < 0){
                 break;
             }
             else if(temp == 0){
-                if(!q.isEmpty())
-                    q.poll();
+                q.remove();
             }
             else{
-                if(q.size() <= N)
-                    q.add(temp);
+                q.offer(temp);
             }
         }
-
-        StringBuilder sb = new StringBuilder();
-        for(int i : q){
-            sb.append(i);
-            sb.append(" ");
+        if(q.isEmpty()) {
+            System.out.println("empty");
+            return;
         }
-        System.out.println(sb.toString());
+        while(!q.isEmpty()){
+            System.out.print(q.remove() + " ");
+        }
     }
 }
