f5cc812537a556130260adf9bcf87b9ef1aeae7d
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2024-08-26 19:00:50 +0900

    [Gold V] Title: AC, Time: 780 ms, Memory: 100196 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,49 +1,65 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
 
+// deque 써서 뒤집을때마다 poll 해줄 방향 바꿔주기 true
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-
-        for (int tc = 1; tc <= T; tc++) {
-            String p = sc.next();
-            int n = sc.nextInt();
-
-            String arrStr = sc.next();
-            Deque<Integer> deque = new LinkedList<>();
-            for (String s : arrStr.substring(1, arrStr.length() - 1).split(","))
-                if (!s.equals(""))
-                    deque.add(Integer.valueOf(s));
-
-            System.out.println(ac(deque, p));
-        }
-    }
-
-    static String ac(Deque<Integer> deque, String commands) {
-        boolean reverse = false;
-
-        for (char command : commands.toCharArray()) {
-            if (command == 'R')
-                reverse = !reverse;
-            else {
-                if (deque.size() == 0)
+    public static String ac(Deque<Integer> dq, char[] cmds){
+        //type R 명령마다 바꿔주기
+        boolean type = true;
+        for(char cmd : cmds){
+            if(cmd == 'R'){
+                type = !type;
+            }
+            else{
+                if(dq.isEmpty())
                     return "error";
 
-                if (reverse)
-                    deque.removeLast();
+                if (type)
+                    dq.pollFirst();
                 else
-                    deque.removeFirst();
+                    dq.pollLast();
             }
-        }
 
+        }
         StringBuilder sb = new StringBuilder("[");
-        while (!deque.isEmpty()) {
-            sb.append(reverse ? deque.removeLast() : deque.removeFirst());
-            if (deque.size() != 0)
-                sb.append(',');
+        while(!dq.isEmpty()){
+            if(type)
+                sb.append(dq.pollFirst());
+            else
+                sb.append(dq.pollLast());
+            if(!dq.isEmpty())
+                sb.append(",");
         }
-        sb.append(']');
-
+        // 맨 마지막 , 콤마 삭제 => 이거 때문에 안되던거였음!!!!!!!!!
+        // sb.delete(sb.length()-1, sb.length());
+        sb.append("]");
         return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        // T가 0이 될때까지라는 뜻
+        while(T-->0){
+            Deque<Integer> dq = new LinkedList<>();
+            char[] cmds = br.readLine().toCharArray();
+            int n = Integer.parseInt(br.readLine());
+
+            // 빈 배열 들어오면 error
+            String cmdStr = br.readLine();
+            // [ ] 떼고
+            String cmdString = cmdStr.substring(1,cmdStr.length()-1);
+            // ,기준으로 숫자 배열 스플릿해서 데크에 넣어줌
+            for(String s : cmdString.split(",")){
+                if(!s.equals(""))
+                    dq.add(Integer.parseInt(s));
+            }
+
+            System.out.println(ac(dq, cmds));
+        }
     }
 }
