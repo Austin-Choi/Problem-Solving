84490386205a20126addce116302eb4befe83e0e
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2025-06-16 18:15:33 +0900

    [Silver IV] Title: 큐, Time: 204 ms, Memory: 19072 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,55 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> s = new ArrayDeque<>();
+
+        for(int n = 0; n<N; n++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+            if(cmd.equals("push")){
+                int num = Integer.parseInt(st.nextToken());
+                s.addLast(num);
+            }
+            else if(cmd.equals("front")){
+                if(!s.isEmpty())
+                    sb.append(s.peekFirst()).append("\n");
+                else
+                    sb.append(-1+"\n");
+            }
+            else if(cmd.equals("back")){
+                if(!s.isEmpty())
+                    sb.append(s.peekLast()).append("\n");
+                else
+                    sb.append(-1+"\n");
+            }
+            else if(cmd.equals("size")){
+                sb.append(s.size()).append("\n");
+            }
+            else if(cmd.equals("empty")){
+                if(s.isEmpty())
+                    sb.append(1+"\n");
+                else
+                    sb.append(0+"\n");
+            }
+            else{
+                if(!s.isEmpty())
+                    sb.append(s.poll()).append("\n");
+                else
+                    sb.append(-1+"\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
