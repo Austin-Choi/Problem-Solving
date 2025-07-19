8b319735b1d147d1160dac18411d8ee1f85b4e7a
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2025-07-19 11:26:21 +0900

    [Gold V] Title: A와 B, Time: 120 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -1,6 +1,15 @@
+/*
+1) 뒤가 A이면 뒤의 A 제거
+2) 뒤가 B이면 뒤의 B 제거하고 뒤집기
+
+연산에 의해 항상 짧아지므로
+길이가 s=t 가 되면
+equals 해보기
+
+StringBuilder 쓰기
+ */
 import java.io.*;
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
