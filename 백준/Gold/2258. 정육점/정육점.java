3fc31928072337ce9ea6e3620c3a93ed0a8f7f2b
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2025-07-25 19:43:00 +0900

    [Gold III] Title: 정육점, Time: 496 ms, Memory: 43812 KB -BaekjoonHub

--- 백준/Gold/2258. 정육점/정육점.java
+++ 백준/Gold/2258. 정육점/정육점.java
@@ -1,66 +1,63 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
     static int N, M;
     static Meat[] board;
-
-    static class Meat implements Comparable<Meat> {
+    static class Meat implements Comparable<Meat>{
         int weight;
         int cost;
-
-        Meat(int w, int c) {
+        Meat(int w, int c){
             this.weight = w;
             this.cost = c;
         }
 
         @Override
-        public int compareTo(Meat o) {
-            if (this.cost == o.cost)
+        public int compareTo(Meat o){
+            if(this.cost == o.cost)
                 return o.weight - this.weight;
             return this.cost - o.cost;
         }
     }
-
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         board = new Meat[N];
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int w = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            board[i] = new Meat(w, c);
+            board[i] = new Meat(w,c);
         }
+        Arrays.sort(board);
 
-        Arrays.sort(board); // cost 오름차순, weight 내림차순
+        int ans = Integer.MAX_VALUE;
+        long sumWeight = 0;
+        int totalCost = 0;
+        boolean found = false;
 
-        int totalWeight = 0;
-        int totalCost = -1;
-        int answer = Integer.MAX_VALUE;
-        boolean isPossible = false;
+        for(int i = 0; i<N; i++){
+            sumWeight += board[i].weight;
 
-        for (int i = 0; i < N; i++) {
-            totalWeight += board[i].weight;
-
-            if (i > 0 && board[i].cost == board[i - 1].cost) {
-                totalCost += board[i].cost; // 같은 가격이면 더 돈이 듬
-            } else {
-                totalCost = board[i].cost; // 새로운 가격이면 처음부터 그 가격 지불
+            if(i>0 && board[i].cost == board[i-1].cost){
+                totalCost += board[i].cost;
+            }
+            else{
+                totalCost = board[i].cost;
             }
 
-            if (totalWeight >= M) {
-                isPossible = true;
-                answer = Math.min(answer, totalCost);
+            if (sumWeight >= M) {
+                found = true;
+                ans = Math.min(ans, totalCost);
             }
         }
+        
+        if(!found)
+            ans = -1;
 
-        bw.write((isPossible ? answer : -1) + "\n");
+        bw.write(String.valueOf(ans));
         bw.flush();
         bw.close();
         br.close();
