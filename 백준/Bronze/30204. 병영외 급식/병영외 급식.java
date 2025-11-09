538ceb739ce7f44b89c491ffd4ff71aa51a268e0
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2025-11-09 13:05:33 +0900

    [Bronze II] Title: 병영외 급식, Time: 116 ms, Memory: 14540 KB -BaekjoonHub

---
+++ 백준/Bronze/30204. 병영외 급식/병영외 급식.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        long X = Long.parseLong(st.nextToken());
+        long tot = 0;
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++)
+            tot += Long.parseLong(st.nextToken());
+        if(tot % X == 0)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+}
