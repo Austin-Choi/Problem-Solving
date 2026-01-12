9966e391834f2063ce75a6465fe9dcd5b1382100
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-01-12 11:02:28 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14192 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Main {
+    static final long r = 31;
+    static final long MOD = 1234567891L;
+    static long[] exptable = new long[51];
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        exptable[0] = 1;
+        for(int i = 1; i<=50; i++){
+            exptable[i] = (exptable[i-1]*r) % MOD;
+        }
+        int L = Integer.parseInt(br.readLine());
+        char[] temp = br.readLine().toCharArray();
+        long hash = 0;
+        for(int i = 0; i<L; i++){
+            hash += ((temp[i]-'a'+1)*exptable[i])%MOD;
+        }
+        System.out.print(hash);
+    }
+}
