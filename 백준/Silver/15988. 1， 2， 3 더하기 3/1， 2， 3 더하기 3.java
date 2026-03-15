8e233f6b0572616d5321d669e3328964b846aa45
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-03-15 12:00:41 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 128 ms, Memory: 22180 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -1,3 +1,18 @@
+
+/*
+상태가 3개니까 벡터로 표현하면 O(3^3 log N)이라서 단일 항목에서는 빠를수도
+T때문에 dp 전처리가 정석일듯
+
+벡터로 나타내면
+i, i-1, i-2일때
+i+1 = i+1, i, i-1
+1행 : 1 1 1
+2행 : 1 0 0
+3행 : 0 1 0
+Vn+1 = 위의 행렬 * Vn
+V3부터 시작 dp[3]까지 정의하고
+ */
+import java.util.*;
 import java.io.*;
 public class Main {
     static int n;
