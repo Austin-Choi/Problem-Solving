82335a30d6660079aed14eec63f3997769d40081
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-04-24 09:17:06 +0900

    Time: 169 ms (27.33%), Space: 48.3 MB (46.35%) - LeetHub

--- 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.java
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.java
@@ -1,12 +1,9 @@
-import java.util.*;
-import java.io.*;
 /*
 그리드 n,m 크기 봐서 
 가능한 가장 큰 롬버스 크기 리밋 구해놓고
 for[가장큰롬버스+1][n][m]
 해서 완탐하기
-dp로는 못하는게 롬버스1이랑 2랑 구성요소가 확장되는게 아니라서 의미가 없고
-m,n 커봐야 50이라 크게잡아도 50 * 50 * 50임
+-> 누적합으로 더 빠르게 가능
 
 롬버스구하기
 1-> 칸하나
@@ -14,119 +11,83 @@ m,n 커봐야 50이라 크게잡아도 50 * 50 * 50임
 꼭지점 4개 먼저 구하고 크기-2한만큼 di,dj 정의해놓은거 따라가면서 더하기
 중간에 unvalid 한 값 들어오면 -1 반환하고 끝내기
 
-서로다른 3개 내림차순
-그냥 다 list에 넣고 sort 할까!
-큐에 넣고 크기 3되면 peek해서 빼고 뭐 그것도 되긴할텐데 중복관리가 안될듯
+누적합으로 O(1)에 구하기 -> / \ 방향으로 2개
+limit 동적으로 구하기
 */
+import java.util.*;
 
 class Solution {
-    // 위 아래 점 특정하고 거기서부터 l-2만큼 이동
-    int[] di = {1,1,-1,-1};
-    int[] dj = {-1,1,-1,1};
     int[][] g;
-    int N;
-    int M;
+    int N, M;
 
-    // size는 1부터 들어옴(size = 대각선 칸 수)
-    // a
-    //b c
-    // d
-    int getRombusSum(int si, int sj, int size){
-        int rst = g[si][sj];
-        int[] a = new int[]{si,sj};
-        int[] b,c,d;
+    int[][] d1; // \ 방향
+    int[][] d2; // / 방향
 
-        // b
-        int ni = si + di[0]*(size-1);
-        int nj = sj + dj[0]*(size-1);
-        if(ni < 0 || nj < 0 || ni >= N || nj >= M)
-            return -1;
-        else{
-            b = new int[]{ni,nj};
-            rst += g[ni][nj];
-        }
+    int getRombusSum(int si, int sj, int k){
+        // si,sj = top
+        int ux = si;
+        int uy = sj;
 
-        ni = si + di[1]*(size-1);
-        nj = sj + dj[1]*(size-1);
-        if(ni < 0 || nj < 0 || ni >= N || nj >= M)
-            return -1;
-        else{
-            c = new int[]{ni,nj};
-            rst += g[ni][nj];
-        }
+        int dx = si + k;
+        int dy = sj;
 
-        ni = si + 2*(size-1); 
-        nj = sj;
-        if(ni < 0 || nj < 0 || ni >= N || nj >= M)
-            return -1;
-        else{
-            d = new int[]{ni,nj};
-            rst += g[ni][nj];
-        }
+        int lx = (si + dx) / 2;
+        int ly = sj - (dx - si) / 2;
 
-        for(int s = 1; s<=size-2; s++){
-            ni = a[0] + s*di[0];
-            nj = a[1] + s*dj[0];
-            if(ni < 0 || nj < 0 || ni >= N || nj >= M)
-                return -1;
-            rst += g[ni][nj];
-        }
+        int rx = (si + dx) / 2;
+        int ry = sj + (dx - si) / 2;
 
-        for(int s = 1; s<=size-2; s++){
-            ni = a[0] + s*di[1];
-            nj = a[1] + s*dj[1];
-            if(ni < 0 || nj < 0 || ni >= N || nj >= M)
-                return -1;
-            rst += g[ni][nj];
-        }
+        if(ly < 0 || ry >= M || dx >= N) return -1;
 
-        for(int s = 1; s<=size-2; s++){
-            ni = d[0] + s*di[2];
-            nj = d[1] + s*dj[2];
-            if(ni < 0 || nj < 0 || ni >= N || nj >= M)
-                return -1;
-            rst += g[ni][nj];
-        }
+        int sum =
+            (d2[lx + 1][ly + 1] - d2[ux][uy + 2]) +
+            (d1[rx + 1][ry + 1] - d1[ux][uy]) +
+            (d1[dx + 1][dy + 1] - d1[lx][ly]) +
+            (d2[dx + 1][dy + 1] - d2[rx][ry + 2]) -
+            (g[ux][uy] + g[dx][dy] + g[lx][ly] + g[rx][ry]);
 
-        for(int s = 1; s<=size-2; s++){
-            ni = d[0] + s*di[3];
-            nj = d[1] + s*dj[3];
-            if(ni < 0 || nj < 0 || ni >= N || nj >= M)
-                return -1;
-            rst += g[ni][nj];
-        }
-        return rst;
+        return sum;
     }
 
     public int[] getBiggestThree(int[][] grid) {
+        g = grid;
         N = grid.length;
         M = grid[0].length;
-        int limit =(int) Math.sqrt(N*N + M*M)+1;
-        g = grid;
 
-        ArrayList<Integer> ansLi = new ArrayList<>();
+        d1 = new int[N + 1][M + 2];
+        d2 = new int[N + 1][M + 2];
 
-        for(int l = 1; l<=limit; l++){
-            for(int i = 0; i<N; i++){
-                for(int j = 0; j<M; j++){
-                    if(l == 1)
-                        ansLi.add(grid[i][j]);
-                    else{
-                        int a = getRombusSum(i,j,l);
-                        if(a != -1)
-                            ansLi.add(a);
-                    }
+        // \ 방향 prefix
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= M; j++){
+                d1[i][j] = d1[i-1][j-1] + g[i-1][j-1];
+                d2[i][j] = d2[i-1][j+1] + g[i-1][j-1];
+            }
+        }
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+
+                // size 0
+                set.add(g[i][j]);
+
+                for(int k = 2; ; k += 2){ // 반드시 2씩 증가
+                    int sum = getRombusSum(i, j, k);
+                    if(sum == -1) break;
+                    set.add(sum);
                 }
             }
         }
 
-        Set<Integer> ss = new HashSet<>(ansLi);
-        List<Integer> aaa = new ArrayList<>(ss);
-        Collections.sort(aaa, Collections.reverseOrder());
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list, Collections.reverseOrder());
 
-        int[] rst = new int[Math.min(aaa.size(), 3)];
-        for(int i = 0; i<Math.min(aaa.size(), 3); i++)
-            rst[i] = aaa.get(i);
+        int[] rst = new int[Math.min(3, list.size())];
+        for(int i = 0; i < rst.length; i++)
+            rst[i] = list.get(i);
+
         return rst;
     }
 }
