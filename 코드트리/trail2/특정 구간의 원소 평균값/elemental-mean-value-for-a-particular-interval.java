88ab0284fa39ac5e491437b3416e8b6c09877619
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-05-12 13:36:01 +0900

    [Passed] 특정 구간의 원소 평균값 (173ms, 19MB)

---
+++ 코드트리/trail2/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+/*
+구간은 연속된것 
+구간 크기 , 시작점 만 다루면 될듯
+
+curSum/가 정확히 나눠떨어질때만 체킹
+*/
+
+public class Main {
+    static int N;
+    static int[] A;
+    static int[] ps;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = new int[N];
+        ps = new int[N+1];
+        for(int i = 0; i<N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+            ps[i+1] = A[i] + ps[i]; 
+        }
+
+        int ans = N;
+        for(int k = 2; k<=N; k++){
+            for(int i = 0; i<=N-k; i++){
+                int curSum = ps[i+k] - ps[i];
+
+                Set<Integer> ss = new HashSet<>();
+                for(int s = i; s<i+k; s++){
+                    ss.add(A[s]);
+                }
+                if(curSum%k == 0 && ss.contains(curSum/k))
+                    ans++;
+            }
+        }
+        System.out.print(ans);
+    }
+}
