f1785dfce03eefc26ecc4098ef605f03777935e6
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-06-17 07:59:44 +0900

    [Passed] 최소 스패닝 트리 4 (569ms, 19MB)

---
+++ 코드트리/trail6/최소 스패닝 트리 4/minimum-spanning-tree-4.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static StreamTokenizer sst = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    static int read() throws IOException{
+        sst.nextToken();
+        return (int) sst.nval;
+    }
+    static char rc() throws IOException{
+        sst.nextToken();
+        return sst.sval.charAt(0);
+    }
+
+    static int N,M;
+    static int[] parent;
+    static boolean[] color;
+    static int sum = 0;
+
+    static int find(int x){
+        if(parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b, int w){
+        int pa = find(a);
+        int pb = find(b);
+        if(pa == pb)
+            return;
+        parent[pb] = pa;
+        sum += w;
+    }
+
+    public static void main(String[] args) throws IOException{
+        N = read();
+        M = read();
+        color = new boolean[N+1];
+        parent = new int[N+1];
+        for(int i= 1; i<=N; i++){
+            parent[i] = i;
+            if(rc() == 'a')
+                color[i] = true;
+        }
+        ArrayList<int[]> li = new ArrayList<>();
+        while(M-->0){
+            int a = read();
+            int b = read();
+            int w = read();
+            if(color[a] != color[b])
+                li.add(new int[]{a,b,w});
+        }
+        Collections.sort(li, (a,b)->{
+            return a[2] - b[2];
+        });
+
+        for(int[] l : li){
+            int u = l[0];
+            int v = l[1];
+            int w = l[2];
+            union(u,v,w);
+        }
+        Set<Integer> s = new HashSet<>();
+        for(int i = 1; i<=N; i++){
+            s.add(find(i));
+            if(s.size() > 1){
+                System.out.print(-1);
+                return;
+            }
+        }
+        System.out.print(sum);
+    }
+}
