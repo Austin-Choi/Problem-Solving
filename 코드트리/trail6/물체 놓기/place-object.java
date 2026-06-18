fb51b101509ede4078daa3d9b8473556bebb4dbb
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-06-19 08:06:48 +0900

    [Passed] 물체 놓기 (201ms, 13MB)

--- 코드트리/trail6/물체 놓기/place-object.java
+++ 코드트리/trail6/물체 놓기/place-object.java
@@ -13,63 +13,72 @@ import java.io.*;
 -----------
 반례 : 직접 설치를 여러번 하는게 더 최적일 수 있음
 -> 가상 정점을 만들어서 설치도 가중치로 넣기
+
+완전그래프의 경우 배열 기반 풀이가 훨씬 빠르다.
+-> 우선순위 큐 풀이 -> N^2 log V
+-> 배열 풀이 -> N^2
 */
 
 public class Main {
-    static StreamTokenizer sst = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    static StreamTokenizer sst =
+        new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
 
-    static int read() throws IOException{
+    static int read() throws IOException {
         sst.nextToken();
         return (int) sst.nval;
     }
 
     static int N;
-    static ArrayList<int[]>[] g;
+    static int[][] cost;
 
-    static long prim(){
-        int s= 0;
-        boolean[] v = new boolean[N+1];
-        // ni, cost
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a->a[1]));
-        pq.add(new int[]{s,0});
+    static long prim() {
+        boolean[] v = new boolean[N + 1];
+        int[] minEdge = new int[N + 1];
+
+        Arrays.fill(minEdge, Integer.MAX_VALUE);
+        minEdge[0] = 0;
+
         long sum = 0;
 
-        while(!pq.isEmpty()){
-            int[] cur = pq.poll();
-            int ci = cur[0];
-            int cd = cur[1];
+        for (int cnt = 0; cnt <= N; cnt++) {
+            int cur = -1;
 
-            if(v[ci])
-                continue;
-            v[ci] = true;
-            sum += cd;
+            // 아직 방문하지 않은 정점 중 가장 싼 정점 선택
+            for (int i = 0; i <= N; i++) {
+                if (!v[i] && (cur == -1 || minEdge[i] < minEdge[cur])) 
+                    cur = i;
+            }
 
-            for(int[] n : g[ci]){
-                pq.add(n);
+            v[cur] = true;
+            sum += minEdge[cur];
+
+            // 비용 갱신
+            for (int next = 0; next <= N; next++) {
+                if (!v[next]) 
+                    minEdge[next] = Math.min(minEdge[next], cost[cur][next]);
             }
         }
+
         return sum;
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         N = read();
-        g = new ArrayList[N+1];
-        for(int i = 0; i<=N; i++)
-            g[i] = new ArrayList<>();
+        cost = new int[N + 1][N + 1];
 
-        for(int i = 0; i<N; i++){
-            int cur = read();
-            g[0].add(new int[]{i+1, cur});
+        // 가상 정점 0 -> i
+        for (int i = 1; i <= N; i++) {
+            int c = read();
+            cost[0][i] = c;
+            cost[i][0] = c;
         }
-        
-        for(int i = 1; i<=N; i++){
-            for(int j = 1; j<=N; j++){
-                int cur = read();
-                if(i == j)
-                    continue;
-                g[i].add(new int[]{j, cur});
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                cost[i][j] = read();
             }
         }
+
         System.out.print(prim());
     }
 }
