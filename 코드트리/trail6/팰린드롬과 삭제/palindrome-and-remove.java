911bd5124594d4a28c7c0e3d97bdc171cf47d1c2
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-06-30 11:45:17 +0900

    [Passed] 팰린드롬과 삭제 (106ms, 10MB)

---
+++ 코드트리/trail6/팰린드롬과 삭제/palindrome-and-remove.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+/*
+연속된게 아니고 전체에서 가장 긴 팰린드롬 길이 찾으면 됨
+그리고 나서 N-dp[0][N-1]
+*/
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] s = br.readLine().toCharArray();
+        int N = s.length;
+        int[][] dp = new int[N][N];
+        for(int i = 0; i<N; i++)
+            dp[i][i] = 1;
+        
+        for(int len = 2; len <= N; len++){
+            for(int i= 0; i+len-1<N; i++){
+                int j = i+len-1;
+                if(s[i] == s[j]){
+                    if(len == 2)
+                        dp[i][j] = Math.max(dp[i][j], 2);
+                    else
+                        dp[i][j] = Math.max(dp[i][j], dp[i+1][j-1]+2);
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        System.out.print(N - dp[0][N-1]);
+    }
+}
