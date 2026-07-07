9935267c2425fa40ef9275e4328105051bde8668
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-07-07 12:28:25 +0900

    [Passed] 최장 릴레이 (178ms, 12MB)

--- 코드트리/trail6/최장 릴레이/longest-relay.java
+++ 코드트리/trail6/최장 릴레이/longest-relay.java
@@ -8,6 +8,8 @@ dp[1<<N][i][c] = 현재 집합상태 m, 마지막으로 방문한 정점 i일때
 -> 사람 수는 불가능 상태를 작성해서 가능상태인 bitCount 값을 최대로 갱신
 -> 가능한지만 알아도 될듯
 -> boolean dp로 하니까 bitcount 가능한 마스크마다 수행해서 느려짐
+
+dp[1<<N][i] = 현재 집합상태 m, 마지막으로 방문한 정점 i일때 최소 cost 
 */
 
 public class Main {
@@ -20,6 +22,7 @@ public class Main {
 
     static int N;
     static int[][] cost;
+    static final int INF = 21;
 
     public static void main(String[] args) throws IOException{
         N = read();
@@ -30,38 +33,35 @@ public class Main {
             }
         }
 
-        int[][][] dp = new int[1<<N][N][21];
-        for(int m = 0; m<(1<<N); m++){
-            for(int i = 0; i<N; i++){
-                Arrays.fill(dp[m][i], -1);
-            }
+        // i를 먼저쓰는게 캐시친화적
+        // 최소 cost 저장하는거니까 max로초기화
+        int[][] dp = new int[N][1<<N];
+        for(int i = 0; i<N; i++){
+            Arrays.fill(dp[i], INF);
         }
-        dp[1<<0][0][0] = 1;
+        dp[0][1<<0] = 0;
 
-        int ans = 0;
-        // 순회 돌 때 많이 걸러지는걸 최상단에 배치해야 함
-        // dp m last lc 가 false일때
         for(int m = 0; m<(1<<N); m++){
-            for(int last = 0; last<N; last++){
+            for(int last = 0; last < N; last++){
                 if((m & (1<<last)) == 0)
                     continue;
-                for(int lc = 0; lc<=20; lc++){
-                    if(dp[m][last][lc] == -1)
+                for(int next = 0; next < N; next++){
+                    if((m & (1<<next)) != 0)
                         continue;
-                    for(int next = 0; next < N; next++){
-                        if((m & (1<<next)) != 0)
-                            continue;
-                        int nc = cost[last][next];
-                        if(nc <= lc)
-                            continue;
-                        int nm = m | (1<<next);
-                        dp[nm][next][nc] = Math.max(dp[nm][next][nc], dp[m][last][lc] + 1);
-                        ans = Math.max(ans, dp[nm][next][nc]);
-                        // bitcount 비용이 너무 큼
-                        // 그냥 dp에 갯수 저장하는 쪽으로 변경
-                        //ans = Math.max(ans, Integer.bitCount(nm));
-                    }
+                    if(cost[last][next] <= dp[last][m])
+                        continue;
+                    int nm = m | (1<<next);
+                    dp[next][nm] = Math.min(dp[next][nm], cost[last][next]);
                 }
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0; i<N; i++){
+            for(int m = 0; m<(1<<N); m++){
+                if(dp[i][m] == INF)
+                    continue;
+                ans = Math.max(ans, Integer.bitCount(m));
             }
         }
         System.out.print(ans);
