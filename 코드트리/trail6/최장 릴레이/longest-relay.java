1c3d92fe9616aad0a7e3c3634bcf62d12751a9a0
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-07-07 12:15:17 +0900

    [Passed] 최장 릴레이 (529ms, 81MB)

--- 코드트리/trail6/최장 릴레이/longest-relay.java
+++ 코드트리/trail6/최장 릴레이/longest-relay.java
@@ -7,6 +7,7 @@ bitCount가 최대가 될때의 그 값
 dp[1<<N][i][c] = 현재 집합상태 m, 마지막으로 방문한 정점 i일때 마지막 방문 cost가 c일때 최대 사람수
 -> 사람 수는 불가능 상태를 작성해서 가능상태인 bitCount 값을 최대로 갱신
 -> 가능한지만 알아도 될듯
+-> boolean dp로 하니까 bitcount 가능한 마스크마다 수행해서 느려짐
 */
 
 public class Main {
@@ -29,8 +30,13 @@ public class Main {
             }
         }
 
-        boolean[][][] dp = new boolean[1<<N][N][21];
-        dp[1<<0][0][0] = true;
+        int[][][] dp = new int[1<<N][N][21];
+        for(int m = 0; m<(1<<N); m++){
+            for(int i = 0; i<N; i++){
+                Arrays.fill(dp[m][i], -1);
+            }
+        }
+        dp[1<<0][0][0] = 1;
 
         int ans = 0;
         // 순회 돌 때 많이 걸러지는걸 최상단에 배치해야 함
@@ -40,19 +46,21 @@ public class Main {
                 if((m & (1<<last)) == 0)
                     continue;
                 for(int lc = 0; lc<=20; lc++){
-                    if(!dp[m][last][lc])
+                    if(dp[m][last][lc] == -1)
                         continue;
                     for(int next = 0; next < N; next++){
                         if((m & (1<<next)) != 0)
                             continue;
-                        if(cost[last][next] == 0)
-                            continue;
-                        if(cost[last][next] <= lc)
+                        int nc = cost[last][next];
+                        if(nc <= lc)
                             continue;
                         int nm = m | (1<<next);
-                        dp[nm][next][cost[last][next]] = true;
-                        ans = Math.max(ans, Integer.bitCount(nm));
-                }
+                        dp[nm][next][nc] = Math.max(dp[nm][next][nc], dp[m][last][lc] + 1);
+                        ans = Math.max(ans, dp[nm][next][nc]);
+                        // bitcount 비용이 너무 큼
+                        // 그냥 dp에 갯수 저장하는 쪽으로 변경
+                        //ans = Math.max(ans, Integer.bitCount(nm));
+                    }
                 }
             }
         }
