f62a00faa1b4d733a3e9c686e270bc32f2c77e3a
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-07-12 10:39:35 +0900

    [Passed] 다른 경로로 이동 (265ms, 15MB)

--- 코드트리/trail5/다른 경로로 이동/move-with-different-path.java
+++ 코드트리/trail5/다른 경로로 이동/move-with-different-path.java
@@ -9,6 +9,13 @@ import java.io.*;
 -> 1번에서 정렬해놔서 사전순 앞선 경로 채택될거임
 -> 이때 i,j를 long형 key로 갖고 w를 value로 갖는 map에 저장
 4. 다시 원래의 그래프에서 1->N 방향으로 최단거리 구하는데 map에 저장된 간선은 피하면서 dist 갱신
+
+----------
+1. N 최대 1000이니까 cost 배열로 중복피해서 만들어주고 그걸로 ArrayList로 그래프 만듬
+2. distSE(1->N) 방향으로 다익한번, distES(N->1) 방향으로 다익한번 돌려줌
+3. while(cur != destination) 에서 distSE[cur] + nd + distES[ni] == distSE[N] 이면 최단경로에 반드시 속함
+4. 3번에서 얻은 간선들 map에 long형 key로 저장하기 
+5. 다시 원래 그래프에서 1->N 방향으로 최단거리 구하는데 3번에서 얻은 간선이면 피하기
 */
 
 public class Main {
