1909c5d3a29f86377da8090f8cef529d6a2bf6f3
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2026-07-20 11:04:40 +0900

    [Passed] 쪼개어 배낭 채우기 구현 (452ms, 21MB)

--- 코드트리/trail5/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.java
+++ 코드트리/trail5/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.java
@@ -15,29 +15,29 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        double[][] arr = new double[N][2];
+        int[][] arr = new int[N][2];
         for(int i = 0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            double w = Double.parseDouble(st.nextToken());
-            double v = Double.parseDouble(st.nextToken());
-            arr[i] = new double[]{w,v};
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            arr[i] = new int[]{w,v};
         }
 
         Arrays.sort(arr, (a,b)->
-            Double.compare(b[1]/b[0] , a[1]/a[0])
+            Double.compare((double)b[1]/b[0] , (double)a[1]/a[0])
         );
 
-        double cur = 0;
+        int cw = 0;
         double val = 0;
         int idx = 0;
-        while(idx < N && cur < M){
-            if(cur + arr[idx][0] <= M){
-                cur += arr[idx][0];
+        while(idx < N && cw < M){
+            if(cw + arr[idx][0] <= M){
+                cw += arr[idx][0];
                 val += arr[idx][1];
             }
             else{
-                val += (M-cur)*(arr[idx][1] / arr[idx][0]);
-                cur = M;
+                val += (M-cw)*((double)arr[idx][1] / arr[idx][0]);
+                cw = M;
             }
             idx++;
         }
